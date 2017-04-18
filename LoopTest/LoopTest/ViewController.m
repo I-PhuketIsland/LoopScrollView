5e2e41a6dd699d7b45d232420ce0f6f5cd2e928e
I-PhuketIsland <347456222@qq.com>
2017-04-18 14:37:10 +0800

    Update ViewController.m

--- LoopTest/LoopTest/ViewController.m
+++ LoopTest/LoopTest/ViewController.m
@@ -17,10 +17,21 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    /*
     LoopScrollView *ss = [[LoopScrollView alloc]initWithFrame:CGRectMake(100, 100, 200, 300) loopTime:3 direction:Rolling_direction_H];
     ss.lSVDelegate = self;
     ss.imageNames = @[@"1.jpg",@"2.jpg",@"3.jpg",@"4.jpg",@"5.jpg"];
     [self.view addSubview:ss];
+     */
+    
+    UIView * view1 = [[UIView alloc]init];
+    view1.backgroundColor = [UIColor blackColor];
+    UIView *view2 = [[UIView alloc]init];
+    view2.backgroundColor = [UIColor redColor];
+    LoopScrollView *sss = [[LoopScrollView alloc]initWithFrame:CGRectMake(0, 400, 200, 300) loopTime:3 direction:Rolling_direction_H];
+    sss.backgroundColor = [UIColor blueColor];
+    sss.imageNames = @[view1,view2];
+    [self.view addSubview:sss];
 }
 
 
